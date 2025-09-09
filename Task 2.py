efcce0f364bd38787da341f15bca469de9081c0a
IsQbal <158292068+IsQbal@users.noreply.github.com>
2025-09-09 11:58:00 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,6 @@
+first_name=input("Enter Your First Name: ")
+last_name=input("Enter Your Last Name: ")
+
+Name=first_name +" "+ last_name
+#print("Hello," ,Name)
+print("Hello, " + Name+"! Welcome to the Python Program.")
